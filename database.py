e8797a1e69853525a5d0d27d0d4e534293705222
Max Lapchuk <makslapchuk@gmail.com>
2026-03-20 12:03:33 +0200

    Configure creation of DB engine and sessions

---
+++ database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+DATABASE_URL = "sqlite:///./travel-planner.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
